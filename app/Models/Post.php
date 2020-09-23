6001e08bf56b8101ca13de52c8a97795cbd5df00
sayeed <sayeed0209@gamil.com>
2020-09-23 23:15:47 +0200

    get user by id and his profile functionality added

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,4 +13,5 @@ class Post extends Model
     {
         $this->belongsTo('User');
     }
+ 
 }
--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -1,7 +1,21 @@
 @extends('layouts.app')
 @section('content')
-  
+<div class="container flex justify-center mt-3">
+    <form action="" method="POST">
+        @csrf
+        <textarea name="body"></textarea>
+        <button type="submit">Add-Post</button>
 
+    </form>
+       
+</div>
+
+
+@foreach ($posts as $post)
+<div>
+   {{$post->body}}
+</div>
+@endforeach
 
 
 
