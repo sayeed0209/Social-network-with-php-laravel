f3e30702761b399c92bd9933d908b43021f7e3c3
sayeed <sayeed0209@gamil.com>
2020-09-25 15:30:50 +0200

    foreign key onDelete(casade) added to the column

--- database/migrations/2020_09_23_170433_create_likes_table.php
+++ database/migrations/2020_09_23_170433_create_likes_table.php
@@ -18,8 +18,8 @@ class CreateLikesTable extends Migration
             $table->unsignedBigInteger('post_id');
             $table->unsignedBigInteger('user_id');
             $table->boolean('type')->default(true);
-            $table->foreign('post_id')->references('id')->on('posts');
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
