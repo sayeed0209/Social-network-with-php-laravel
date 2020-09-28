d54841e139d01f87cf1f52040ba2d1fd84dccdb2
sayeed <sayeed0209@gamil.com>
2020-09-28 12:06:56 +0200

    route has been created for comment

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\FriendController;
 use App\Http\Controllers\LikeController;
 use Illuminate\Support\Facades\Auth;
@@ -60,7 +61,8 @@ Route::post('/showuser', [UserController::class, 'show_notYetFriends']);
 Route::post('/search_user', [UserController::class, 'search_user']);
 Route::get('/showFriends', [FriendController::class, 'index']);
 Route::get('/add_friend/{user_id}', [FriendController::class, 'addFriend']);
-
+// comments
+Route::post('/comment',[CommentController::class,'index'])->middleware(['auth:sanctum', 'verified']);
 Route::get('/updateProfile', [UserController::class, 'edit'])->middleware(['auth:sanctum', 'verified']);
 Route::post('/updateProfile', [UserController::class, 'update'])->middleware(['auth:sanctum', 'verified']);
 Route::get('/home', [PostController::class, 'getAllowedPosts'])->middleware(['auth:sanctum', 'verified']);
